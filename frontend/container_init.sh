dda47585afb2c59fa76c5605d8e209960049c3ea
Michael Barrientos <mbarrientos@chanzuckerberg.com>
2020-10-16 08:27:05 -0700

    Fix frontend links, configs

--- frontend/container_init.sh
+++ frontend/container_init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ln -s /opt/node_app/node_modules/* ./node_modules/.
+ln -sf /opt/node_app/node_modules/* ./node_modules/.
+cp src/configs/local.js src/configs/configs.js
 gatsby develop --host 0.0.0.0
