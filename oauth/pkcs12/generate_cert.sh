76b5016513c130fb1399a9b6143a30cf4e599298
Jessica Gadling <jgadling@chanzuckerberg.com>
2020-10-22 11:48:06 -0700

    Add a SAN to the cert.

--- oauth/pkcs12/generate_cert.sh
+++ oauth/pkcs12/generate_cert.sh
@@ -1,2 +1,6 @@
-openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout server.key -out server.crt
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3560 -nodes -keyout server.key -out server.crt -subj '/CN=localhost' -extensions san -config <( \
+  echo '[req]'; \
+  echo 'distinguished_name=req'; \
+  echo '[san]'; \
+  echo 'subjectAltName=DNS:localhost')
 openssl pkcs12 -export -out certificate.pfx -inkey server.key -in server.crt
