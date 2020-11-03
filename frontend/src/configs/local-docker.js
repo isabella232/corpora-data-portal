a4a824957d060f2c70bf67bc6ac6c9ef665a4a3d
Jessica Gadling <jgadling@chanzuckerberg.com>
2020-11-03 14:43:24 -0800

    e2e tests still pass without the backend alias

--- frontend/src/configs/local-docker.js
+++ frontend/src/configs/local-docker.js
@@ -1,8 +1,5 @@
-// (thuang): For local development, please copy the content of this file
-// to a new file named `configs.js` in this directory.
-
 const configs = {
-  API_URL: "http://backend:5000",
+  API_URL: "http://localhost:5000",
 };
 
 if (typeof module !== "undefined") module.exports = configs;
